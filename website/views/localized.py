cf589a6e7ad4a26daeef5d7c0eb8311f6725a85f
Stefane Fermigier <sf@fermigier.com>
2013-08-27 09:43:57 +0200

    Don't break when there is less than 12 speakers.

--- website/views/localized.py
+++ website/views/localized.py
@@ -117,7 +117,11 @@ def home():
   template = "index.html"
   page = {'title': 'Open World Forum 2013'}
   news = get_news(lang=g.lang, limit=6)
-  speakers = random.sample(Speaker.query.all(), 12)
+  speakers = Speaker.query.all()
+  if len(speakers) >= 12:
+    speakers = random.sample(speakers, 12)
+  else:
+    speakers = []
   blocks = get_blocks(g.lang)
   return render_template(template,
                          page=page, news=news, speakers=speakers, blocks=blocks)
